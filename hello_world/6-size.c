b092edb38fc9ea67cac23e281f88f1b8a3159a3f
Bluebee45 <santi72143@gmail.com>
2023-02-17 08:08:24 -0800

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ *
+ * main -C program that prints the size of various types on the computer
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+char carater;
+int entero;
+long int longEntero;
+long long int llEnt;
+float flotante;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(carater));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(entero));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longEntero));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llEnt));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(flotante));
+return (0);
+}
