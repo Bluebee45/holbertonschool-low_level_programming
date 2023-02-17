5da940bc6035cdf2e791fb8b77781238b20440c4
Bluebee45 <santi72143@gmail.com>
2023-02-16 19:37:24 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,4 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
  *
  * *main - a C program that prints exactly "Programming is like building
@@ -8,13 +6,8 @@
  * *Return: 0 (Success)
  *
  * */
-
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
