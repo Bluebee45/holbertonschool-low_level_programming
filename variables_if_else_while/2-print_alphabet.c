20c18b6b275cb28c57c824388190e5995fc58e74
Bluebee45 <santi72143@gmail.com>
2023-02-20 15:42:11 -0800

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * description - print alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lt = 'a';
+	for (lt = 'a'; lt <= 'z'; lt++);
+       putchar (lt);
+putchar ('\n')
+
+return (0)	
+}
