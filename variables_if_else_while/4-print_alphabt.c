79ad58dbeebcced2cee62d3ef33429a8b53cbbf3
Bluebee45 <santi72143@gmail.com>
2023-02-20 17:52:21 -0800

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - function
+ * description alphabt print
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char letters;
+for (letters = 'a'; letters <= 'z'; letters++)
+putchar(letters);
+{
+if (letters != 'e', &&letters != 'q')
+}
+putchar('\n');
+return (0);
+}
