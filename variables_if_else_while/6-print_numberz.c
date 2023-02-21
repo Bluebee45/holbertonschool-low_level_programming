c83bfc8dc0d2e1202e89de11abaacb3cfb1d6f38
Bluebee45 <santi72143@gmail.com>
2023-02-20 20:01:16 -0800

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * description - print numberz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+putchar(num);
+putchar('\n');
+return (0);
+}
