497475cb4b8b8e3bc9df03158bf71b02ebc75024
Bluebee45 <santi72143@gmail.com>
2023-02-22 13:22:30 -0800

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,9 @@
 void print_alphabet_x10(void)
 {
 char x, u;
-for (u = 0; u <= 9; x++)
+for (u = 0; u <= 9; u++)
 {
-for (x = 'a'; x <= 'z'; u++)
+for (x = 'a'; x <= 'z'; x++)
 {
 putchar('x');
 }
