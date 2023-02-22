3add41e4941a1c16a9bb2f2efe6cfb49278a60cf
Bluebee45 <santi72143@gmail.com>
2023-02-22 10:39:28 -0800

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - function print alphabet
+ * description - pritn the alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+}
