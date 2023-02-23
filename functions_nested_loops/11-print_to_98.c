ce288bf268d129e39e5a3e7ff6a7e12cb96914cc
Bluebee45 <santi72143@gmail.com>
2023-02-23 08:00:20 -0800

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,13 +14,13 @@ void print_to_98(int n)
 if (n >= 98)
 {
 while (n > 98)
-printf("%d", n--);
+printf("%d, ", n--);
 printf("%d", n);
 }
 else
 {
 while (n < 98)
-printf("%d", n++);
+printf("%d, ", n++);
 printf("%d", n);
 }
 }
