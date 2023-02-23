63612341be9082103360bea8beffbc20302b02ac
Bluebee45 <santi72143@gmail.com>
2023-02-22 16:00:23 -0800

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - function
+ * description - print the sing oh a number
+ * @n: is the argument of the function
+ *
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
