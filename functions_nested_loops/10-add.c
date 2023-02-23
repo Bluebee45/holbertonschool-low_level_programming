72c84a53399007ab75503f3fa85743752834d92b
Bluebee45 <santi72143@gmail.com>
2023-02-23 07:05:33 -0800

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function
+ * description - adds two intengers
+ * @num1: is the argument for the function int
+ * @num2: is the argument for the function int
+ * Return: 0
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
