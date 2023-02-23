afacd7a244eac08720f5ca4263f3807b0984d5ed
Bluebee45 <santi72143@gmail.com>
2023-02-22 21:02:24 -0800

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,8 +16,8 @@ for (b = 0; b <= 9; b++)
 c = a * b;
 if (c > 9)
 {
-	d = c % 10;
-	e = (c - d) / 10;
+d = c % 10;
+e = (c - d) / 10;
 _putchar(44);
 _putchar(32);
 _putchar(d + '0');
@@ -25,16 +25,14 @@ _putchar(e + '0');
 }
 else
 {
-	if (b != 0)
-	{
-		_putchar(44);
-		_putchar(32);
-		_putchar(32);
-	}
+if (b != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
 _putchar(c + '0');
 }
 }
 _putchar('\n');
 }
-}
-
