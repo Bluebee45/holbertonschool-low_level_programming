ef2e0191340c8ba08ad9a2ec5764872f76225ff5
Bluebee45 <santi72143@gmail.com>
2023-02-27 20:41:01 -0800

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -2,15 +2,15 @@
 
 /**
  * print_rev - function
- * description - print a string in reverse
- * @s: is the argument for the function
- *
+ * @s: Argument for the function and parameter
+ * Return: Always 0.
  */
-
 void print_rev(char *s)
 {
 int c;
 for (c = 0; s[c] != 0; c++)
+{
+}
 for (c = c - 1; c >= 0; c--)
 _putchar(s[c]);
 _putchar('\n');
