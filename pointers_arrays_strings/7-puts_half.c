292ca716f22c5dcaa7a1fb99765f306cd007ddc1
Holberton <holberton@Holbertons-iMac.local>
2023-02-28 14:41:14 -0500

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - function
+ * @str: The string to be printe argument
+ * description - prints half of a string
+ */
+void puts_half(char *str)
+{
+	int index = 0, len = 0, n;
+
+	while (str[index++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
