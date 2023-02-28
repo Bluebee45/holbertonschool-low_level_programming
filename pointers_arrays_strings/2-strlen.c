7a12308825c1764b128301f93c810c64439c6940
Bluebee45 <santi72143@gmail.com>
2023-02-27 18:18:08 -0800

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function
+ * description - returns the legth of a string
+ * @s: is the argument for the function
+ *
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
+}
