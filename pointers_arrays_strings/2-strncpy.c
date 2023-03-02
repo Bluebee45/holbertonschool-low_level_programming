52fb905d37c6f74622534f246d5d803328c26c35
Bluebee45 <santi72143@gmail.com>
2023-03-02 14:33:43 -0800

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-		int i;
-		for (i = 0; src[i] != n; i++)
-		{
-				dest[i] = src[i];
-		}
-		dest[i++] = n;
-		return (dest);
+	int index = 0, src_len = 0;
+while (src[index++])
+src_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[index] = src[index];
+for (index = src_len; index < n; index++)
+dest[index] = '\0';
+return (dest);
 }
