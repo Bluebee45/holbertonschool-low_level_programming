032944b18101a1079ee41fbaf8511436058c1372
Holberton <holberton@Holbertons-iMac.local>
2023-03-06 15:09:21 -0500

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function
+ * description - prints a string in reverse
+ */
+
+void _print_rev_recursion(char *s)
+{
+		int c;
+for (c = 0; s[c] != 0; c++)
+{
+}
+for (c = c - 1; c >= 0; c--)
+_putchar(s[c]);
+_putchar('\n');
+}
