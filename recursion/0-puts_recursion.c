e21cb9409cf8a9ba7af3c7e29f4ffad458034a2b
Holberton <holberton@Holbertons-iMac.local>
2023-03-06 13:56:51 -0500

    0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function
+ * description - prints a string
+ * @s: argument for the function
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+		printf("%s\n", s);
+}
